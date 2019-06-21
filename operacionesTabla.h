9037cdac992cddab9367615c3ea25b4ccc324295
Sebastian <seba.benamo@gmail.com>
2019-06-21 11:36:50 -0300

    Me falto agregar operacionesTabla.h

---
+++ operacionesTabla.h
@@ -0,0 +1,14 @@
+#ifndef OPERACIONESTABLA_H_
+#define OPERACIONESTABLA_H_
+
+#include <stdio.h>
+
+void optabla_agregar(char * filename, char * path, int bitValido, char * IP);
+
+int optabla_modificar(char * filename, char * path, char * newPath, int bitValido, char * IP);
+
+int optabla_suprimir(char * filename, char * path);
+
+int optabla_leer(char * filename);
+
+#endif
